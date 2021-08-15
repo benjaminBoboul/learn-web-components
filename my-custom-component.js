0dfd300c9aa5eb97b1d9ed0c6315dd42ac496766
Benjamin Boboul <benjamin.boboul@protonmail.com>
2021-08-15 11:55:24 +0200

    Add index.html and extends our custom element from HTMLParagraphElement

--- my-custom-component.js
+++ my-custom-component.js
@@ -1,9 +1,16 @@
-class CardElement extends HTMLElement {
+class CardElement extends HTMLParagraphElement {
+
+  constructor () {
+    super()
+
+    this.setAttribute("class", "antialiased")
+  }
+
   connectedCallback() {
     console.debug("Add CardEelement to the page")
   }
 }
 
 if (!customElements.get("some-element")) {
-  customElements.define("some-element", CardEelement)
+  customElements.define("some-element", CardElement, {extends: "p"})
 }
