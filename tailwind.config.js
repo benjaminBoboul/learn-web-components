b6f5fd4c8204796424ed33aeec205744a8b9e045
Benjamin Boboul <benjamin.boboul@protonmail.com>
2021-10-07 22:36:44 +0200

    Start using tailwindcss cli

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    "./components/*.js",
+    "./components/*.html"
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
